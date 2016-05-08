3d3fc3fb00d854a11a75bb3748fc92783769f834
zb d <zb d>
2016-05-08 21:39:35 +0800

    添加接口判断

--- apiJsonp.js
+++ apiJsonp.js
@@ -1,15 +1,24 @@
-var http = require("http"); 
+var http = require("http");
+var url = require('url');
  
-http.createServer(function(request, response) { 
- 
-    response.writeHead(200, {"Content-Type": "text/plain"}); 
-    var res = {
-    	msg:'',
-    	data:'Hello World',
-    	code:1
-    }
-    response.write('k('+JSON.stringify(res)+');'); 
+http.createServer(function(request, response) {
+	// 
+	var path = (url.parse(request.url).pathname).split('/')[1];
+	console.log(path);
+	if(path == 'jsonpTest'){
+	    response.writeHead(200, {"Content-Type": "text/plain"});
+	    var res = {
+	    	msg:'',
+	    	data:'Hello World',
+	    	code:1
+	    }
+	    response.write('k('+JSON.stringify(res)+');'); 
+	}else{
+		response.write('k("request error");');
+	}
  
     response.end(); 
  
-}).listen(1234);
+}).listen(1234,function () {
+	console.log("listen on port 1234......");
+});
